fd10582d03db5a91f9435ccb1f317d7cfe04e68d
HenryChaing <146239606+HenryChaing@users.noreply.github.com>
2023-09-28 03:20:08 +0800

    updata function pointer

--- main.c
+++ main.c
@@ -5,19 +5,26 @@ int plus(int a, int b) { return a + b; }
 int minus(int a, int b) { return a - b; }
 int multiply(int a, int b) { return a * b; }
 int divided(int a, int b) { return a / b; }
+
+int(*func_ptr_arr[4])(int,int);
+
 int main()
 {
 	int a, c;
 	char b;
+	func_ptr_arr[0] = plus;
+	func_ptr_arr[1] = minus;
+	func_ptr_arr[2] = multiply;
+	func_ptr_arr[3] = divide;
 	printf("key a Function \nEX: 1 + 1\n");
 	scanf("%i %c %i", &a, &b, &c);
 	//IF ELSE IF
 	if (b == '+')
-		printf("%d %c %d = %d\n", a, b, c, plus(a, c));
+		printf("%d %c %d = %d\n", a, b, c, (*func_ptr_arr[0])(a,c));
 	else if (b == '-')
-		printf("%d %c %d = %d\n", a, b, c, minus(a, c));
+		printf("%d %c %d = %d\n", a, b, c, (*func_ptr_arr[1])(a,c));
 	else if (b == '*')
-		printf("%d %c %d = %d\n", a, b, c, multiply(a, c));
+		printf("%d %c %d = %d\n", a, b, c, (*func_ptr_arr[2])(a,c));
 	else if (b == '/')
-		printf("%d %c %d = %d\n", a, b, c, divided(a, c));
+		printf("%d %c %d = %d\n", a, b, c, (*func_ptr_arr[3])(a,c));
 }
